6c5a03de3c4294852c50279cdffc727570484a25
inzamulhaque <mdihalif@gmail.com>
2026-04-19 18:16:54 +0600

    create auth middleware

--- backend/src/app/middlewares/auth.ts
+++ backend/src/app/middlewares/auth.ts
@@ -1,13 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/AppError.js";
+import { jwtUtils } from "../utils/jwt.js";
 
 const auth = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (
+    req: Request & { user?: { email: string } },
+    res: Response,
+    next: NextFunction,
+  ) => {
     const token = req.headers.authorization;
 
     if (!token) {
-      return new AppError("Unauthorized", 401);
+      return new AppError("Unauthorized!", 401);
     }
+
+    const decoded = jwtUtils.verifyToken(token);
+    req.user = decoded as { email: string };
+
+    next();
   },
 );
+
+export default auth;
