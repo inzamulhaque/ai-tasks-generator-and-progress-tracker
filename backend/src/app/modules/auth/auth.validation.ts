6c11344c82d0889579dad9345021a6ee7c7bdbd0
inzamulhaque <mdihalif@gmail.com>
2026-04-19 18:26:28 +0600

    create siguo user validation schema and interface

---
+++ backend/src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const validateSignUpWithPasswordSchema = z.object({
+  body: z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.templateLiteral([z.string().min(1), "@", z.string().max(64)]),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+  }),
+});
