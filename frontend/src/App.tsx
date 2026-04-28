daac085e435efa5961740b0bb51bcab84ddd421f
inzamulhaque <mdihalif@gmail.com>
2026-04-28 13:04:02 +0600

    create dark and light theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useTheme } from "next-themes";
+import { Button } from "./components/ui/button";
+import { Monitor, Moon, Sun } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { setTheme } = useTheme();
 
   return (
     <>
+      <div className="flex gap-2">
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => setTheme("light")}
+          className="transition-all duration-300 hover:scale-110 active:scale-95"
+        >
+          <Sun className="h-5 w-5" />
+        </Button>
+
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => setTheme("dark")}
+          className="transition-all duration-300 hover:scale-110 active:scale-95"
+        >
+          <Moon className="h-5 w-5" />
+        </Button>
+
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => setTheme("system")}
+          className="transition-all duration-300 hover:scale-110 active:scale-95"
+        >
+          <Monitor className="h-5 w-5" />
+        </Button>
+      </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +62,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </NextThemesProvider>
+  );
+};
+
+export default ThemeProvider;
